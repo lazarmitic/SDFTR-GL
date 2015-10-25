44d6db089a4eebacd5ba16cc9e41df8b47a8d02f
Lazar Mitić <Lazar__93@hotmail.com>
2015-10-25 16:48:21 +0100

    Add - GLFW Window
    Add glfw window initialization code in main.cpp/main function.

--- SDFTR-GL - VS Solution/SDFTR-GL - VS Project/main.cpp
+++ SDFTR-GL - VS Solution/SDFTR-GL - VS Project/main.cpp
@@ -1,8 +1,40 @@
 #include <iostream>
 
+#include <GLFW/glfw3.h>
+
 int main(int argc, char * argv[])
 {
-	std::cout << "Stage: VS Project creted." << std::endl;
+	std::cout << "Stage: GLFW window initialized." << std::endl;
 
+	/// GLEW Window initialization 
+	int windowWidth = 800; 
+	int windowHeight = 600;
+	char * windowName = "Signed Distance Field Text Rendering";
+	GLFWwindow *windowHandle;
+
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+
+	windowHandle = glfwCreateWindow(windowWidth, windowHeight, windowName, nullptr, nullptr);
+	glfwMakeContextCurrent(windowHandle);
+	///
+	
+	/// Render loop
+	while (!glfwWindowShouldClose(windowHandle))
+	{
+		//Render code goes here
+
+		glfwSwapBuffers(windowHandle);
+		glfwPollEvents();
+	}
+	///
+
+	/// Free memory
+	glfwDestroyWindow(windowHandle);
+	///
+	
 	return 0;
 }
