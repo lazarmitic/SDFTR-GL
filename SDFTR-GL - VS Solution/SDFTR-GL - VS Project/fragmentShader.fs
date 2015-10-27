d26d1b9ba3320156b623e0524b847eb99b4e81b4
Lazar Mitić <Lazar__93@hotmail.com>
2015-10-27 11:32:55 +0100

    Add - Render Font Map as Text
    Updated fragment shader to render font map as text.

--- SDFTR-GL - VS Solution/SDFTR-GL - VS Project/fragmentShader.fs
+++ SDFTR-GL - VS Solution/SDFTR-GL - VS Project/fragmentShader.fs
@@ -6,7 +6,11 @@ out vec4 color;
 
 uniform sampler2D ourTexture;
 
+const float smoothing = 0.1;
+
 void main()
 {
-    color = texture(ourTexture, TexCoord);
+      float distance = texture2D(ourTexture, TexCoord).a;
+      float alpha = smoothstep(0.5 - smoothing, 0.5 + smoothing, distance);
+      color = vec4(1.0, 1.0, 1.0, alpha);
 }
